c00c8f3354b50d2f792c2759612c74bf868b04a0
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2018-02-27 10:01:50 -0700

    initial commit

---
+++ jj.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sam.math_util import avk, deriv_avk
+from sam.optimize import check_grad
+
+#from sam.corpus.corpus import CorpusReader
+#
+# from sam.vem.model import VEMModel
+
+#import sam.log as log
