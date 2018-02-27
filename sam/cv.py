2c08dcb2bfe1e0a6615c4e8dc30ca6c2b12c42f5
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2018-02-27 10:56:37 -0700

    Revert "initial commit"
    This reverts commit c00c8f3354b50d2f792c2759612c74bf868b04a0.

--- sam/cv.py
+++
@@ -1,5 +0,0 @@
-import sys
-from sam.weka.cross_validator_cli import CrossValidationTask
-
-if __name__ == '__main__':
-    CrossValidationTask(sys.argv)
--- sam/vem/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from sam.vem.cli import VEMTask
-
-if __name__ == '__main__':
-    VEMTask(sys.argv)
